140dc76f490345b7597456086d41567ece02eb4c
Eljas <eljas.roellin@gmail.com>
2022-12-05 21:52:25 +0100

    restored working vanilla example and count tests

--- test_file.py
+++ test_file.py
@@ -2,6 +2,10 @@ def func(x):
     return x + 1
     
 def test_answer():
-    assert func(3) == 5 # func will actually return 4, as it should
+    assert func(3) == 4
+    assert func(4) == 5
+    assert func(5) == 6
     
+def test_answerII()
+    assert func(6) == 7
 
